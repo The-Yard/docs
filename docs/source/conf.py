25d2024b98b3dba5adbfe34cc42c63b49e616401
EvilTerabite <48357426+EvilTerabite@users.noreply.github.com>
2023-07-28 01:30:05 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'The Yard'
+copyright = '2023, The Yard'
+author = 'Jeremy'
 
-release = '0.1'
-version = '0.1.0'
+release = 'v1'
+version = '0.1a'
 
 # -- General configuration
 
